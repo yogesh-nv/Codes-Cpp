a1fe766b28dace0efef99382d1a17c541e215c0b
yogeshnv <yogeshnv9774@gmail.com>
2024-09-26 18:55:12 +0530

    update

--- Codes/Codes/codes/main.cpp
+++ Codes/Codes/codes/main.cpp
@@ -7,8 +7,9 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    string s1="yogesh";
+    int n=s1.size();
+    subString(s1,n);
     return 0;
 }
